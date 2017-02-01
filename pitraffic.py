361f76e335bd56d21f1e9775aa5361573eca470f
Thymo van Beers <thymovanbeers@gmail.com>
2017-02-01 17:37:26 +0100

    Add state tracking.

--- pitraffic.py
+++ pitraffic.py
@@ -13,10 +13,15 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
 
 from time import sleep
+from collections import deque
 import RPi.GPIO as GPIO
 
 GPIO_inputs = [26,19,13,6,5,11]
 GPIO_outputs = [16,20,21,12,7,8,25,24,23,18,2,3,4,17,27,22,10,9]
+queue = deque([])
+red_state = []
+amber_state = [1,2,3,4,5,6]
+green_state = []
 
 def setup():
     GPIO.setmode(GPIO.BCM) # Use BCM chip numbering
@@ -28,34 +33,80 @@ def setup():
 def sensor_event(channel):
     if GPIO.input(channel):
         print("Input went high: ", channel)
+        queue.insert(channel) # kijken als hij al instaat!
     else:
         print("Input went low: ", channel)
+        #queue. Weghalen?
+
+def priority(i):
+    if i == 1:
+        
+    elif i == 2:
+        pass
+    elif i == 3:
+        pass
+    elif i == 4:
+        pass
+    elif i == 5:
+        pass
+    elif i == 6:
+        pass
 
 def red(n):
     if n == 1:
         GPIO.output(16, True)
         GPIO.output(20, False)
         GPIO.output(21, False)
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        red_state.remove(red_state.remove(n)
+        red_state.append(n)
+
     elif n == 2:
         GPIO.output(8, True)
         GPIO.output(7, False)
         GPIO.output(12, False)
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        red_state.remove(red_state.remove(n)
+        red_state.append(n)
+        
     elif n == 3:
         GPIO.output(23, True)
         GPIO.output(24, False)
         GPIO.output(25, False)
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        red_state.remove(red_state.remove(n)
+        red_state.append(n)
+        
     elif n == 4:
         GPIO.output(2, True)
         GPIO.output(3, False)
         GPIO.output(18, False)
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        red_state.remove(red_state.remove(n)
+        red_state.append(n)
+        
     elif n == 5:
         GPIO.output(4, True)
         GPIO.output(17, False)
         GPIO.output(27, False)
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        red_state.remove(red_state.remove(n)
+        red_state.append(n)
+        
     elif n == 6:
         GPIO.output(22, True)
         GPIO.output(10, False)
         GPIO.output(9, False)
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        red_state.remove(red_state.remove(n)
+        red_state.append(n)
+        
     else:
         print("Invalid identifier!")
     print("Light change: Red ", n)
@@ -65,26 +116,56 @@ def amber(n):
         GPIO.output(16, False)
         GPIO.output(20, True)
         GPIO.output(21, False)
+        red_state.remove(red_state.index(n))
+        green_state.remove(green_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        amber_state.append(n)
+
     elif n == 2:
         GPIO.output(8, False)
         GPIO.output(7, True)
         GPIO.output(12, False)
+        red_state.remove(red_state.index(n))
+        green_state.remove(green_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        amber_state.append(n)
+
     elif n == 3:
         GPIO.output(23, False)
         GPIO.output(24, True)
         GPIO.output(25, False)
+        red_state.remove(red_state.index(n))
+        green_state.remove(green_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        amber_state.append(n)
+
     elif n == 4:
         GPIO.output(2, False)
         GPIO.output(3, True)
         GPIO.output(18, False)
+        red_state.remove(red_state.index(n))
+        green_state.remove(green_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        amber_state.append(n)
+
     elif n == 5:
         GPIO.output(4, False)
         GPIO.output(17, True)
         GPIO.output(27, False)
+        red_state.remove(red_state.index(n))
+        green_state.remove(green_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        amber_state.append(n)
+
     elif n == 6:
         GPIO.output(22, False)
         GPIO.output(10, True)
         GPIO.output(9, False)
+        red_state.remove(red_state.index(n))
+        green_state.remove(green_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        amber_state.append(n)
+
     else:
         print("Invalid identifier!")
     print("Light change: Amber ", n)
@@ -94,26 +175,56 @@ def green(n):
         GPIO.output(16, False)
         GPIO.output(20, False)
         GPIO.output(21, True)
+        red_state.remove(red_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        green_state.append(n)
+
     elif n == 2:
         GPIO.output(8, False)
         GPIO.output(7, False)
         GPIO.output(12, True)
+        red_state.remove(red_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        green_state.append(n)
+
     elif n == 3:
         GPIO.output(23, False)
         GPIO.output(24, False)
         GPIO.output(25, True)
+        red_state.remove(red_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        green_state.append(n)
+
     elif n == 4:
         GPIO.output(2, False)
         GPIO.output(3, False)
         GPIO.output(18, True)
+        red_state.remove(red_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        green_state.append(n)
+
     elif n == 5:
         GPIO.output(4, False)
         GPIO.output(17, False)
         GPIO.output(27, True)
+        red_state.remove(red_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        green_state.append(n)
+
     elif n == 6:
         GPIO.output(22, False)
         GPIO.output(10, False)
         GPIO.output(9, True)
+        red_state.remove(red_state.index(n))
+        amber_state.remove(amber_state.index(n))
+        green_state.remove(green_state.index(n))
+        green_state.append(n)
+
     else:
         print("Invalid identifier!")
     print("Light change: Green ", n)
@@ -126,29 +237,9 @@ try:
     GPIO.add_event_detect(6, GPIO.BOTH, callback=sensor_event, bouncetime=1000)
     GPIO.add_event_detect(5, GPIO.BOTH, callback=sensor_event, bouncetime=1000)
     GPIO.add_event_detect(11, GPIO.BOTH, callback=sensor_event, bouncetime=1000)
-    while True:
-        break
-        red(1)
-        red(2)
-        red(3)
-        red(4)
-        red(5)
-        red(6)
-        sleep(3)
-        amber(1)
-        amber(2)
-        amber(3)
-        amber(4)
-        amber(5)
-        amber(6)
-        sleep(3)
-        green(1)
-        green(2)
-        green(3)
-        green(4)
-        green(5)
-        green(6)
-        sleep(3)
+    while True: # Main program loop
+        
+        
     while True:
         sleep(1)
 finally:
